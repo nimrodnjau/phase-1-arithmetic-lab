1c4b0e8af3aa2a335a6a0a47a7c9a00b40b329e8
nimrod njau <nimrodnjau@gmail.com>
2024-03-23 18:39:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random()*10000)+1
+
+const num3 = 14
+const num4 = 5
+const mod = num3%num4
+
+const max = (Math.max(5, 10, 15, 20))
